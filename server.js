f32e071f108db3a9fd27ca31d7388673e49d8bf1
Thanmai Chowhan <24wh1a05d0@bvrithyderabad.edu.in>
2026-07-03 13:45:11 +0530

    update

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 app.use(express.static('frontend'));    //serve static files
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(port, () => {
     console.log(`Success! Server is running on http://localhost:${port}`);
 });
